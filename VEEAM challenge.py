10f7f9ae1c353470778f818f72ea22c4c00bd6db
Eleuthar <flbujoreanu@gmail.com>
2023-04-21 20:05:52 +0300

    Update VEEAM challenge.py

--- VEEAM challenge.py
+++ VEEAM challenge.py
@@ -10,31 +10,31 @@ from pdb import set_trace
 
 
 __doc__ = '''
-    Usage: python dirSync.py <source_path> <destination_path> <integer> <S||M||H||D> <log_path>
-    S = SECONDS
-    M = MINUTES
-    H = HOURS
-    D = DAYS
-    
-    Example for synchronizing every 5 minutes with absolute path:   
-    $ python dir_sync.py "C:\\Users\\MrRobot\\Documents\\Homework\\" "C:\\Users\\MrRobot\\Downloads\\VEEAM_CLOUD\\Homework\\" 5 M "C:\\Program Files\\VEEAM\\logs\\"
-    
-    Example for synchronizing every 5 seconds with relative path:
-    $ python ..\\INFOSEC .\\INFOSEC 5 S .\\logz
-    '''
-    
+	Usage: python dirSync.py <source_path> <destination_path> <integer> <S||M||H||D> <log_path>
+	S = SECONDS
+	M = MINUTES
+	H = HOURS
+	D = DAYS
+	
+	Example for synchronizing every 5 minutes with absolute path:   
+	$ python dir_sync.py "C:\\Users\\MrRobot\\Documents\\Homework\\" "C:\\Users\\MrRobot\\Downloads\\VEEAM_CLOUD\\Homework\\" 5 M "C:\\Program Files\\VEEAM\\logs\\"
+	
+	Example for synchronizing every 5 seconds with relative path:
+	$ python ..\\INFOSEC .\\INFOSEC 5 S .\\logz
+	'''
+	
 
 if len( argv ) != 6 or not argv[3].isdigit() or argv[4].upper() not in [ 'S','M','H','D' ]:
-    print( __doc__ )
-    exit()
-    
-    
-# global variables    
+	print( __doc__ )
+	exit()
+	
+	
+# global variables	
 timeframe = { 
-    "S" : 1,
-    "M" : 60,
-    "H" : 3600,
-    "D" : 86400
+	"S" : 1,
+	"M" : 60,
+	"H" : 3600,
+	"D" : 86400
  }
 client = argv[1]
 cloud = argv[2]
@@ -44,273 +44,256 @@ log_path = argv[5]
 client_hexmap = { }
 cloud_hexmap = { }
 
-    
+	
 def setup_log_path( log_path ):
 # implemented log path validation since user interpretation can be ambiguous: either provide a directory to be created or use an existing one.
-    
-    print( f'Validating log directory: "{ log_path }"\n' )    
-    # set a unique delimiter regardless of platform (Linux\Windows)
-    
-    # folder check
-    if not path.exists( log_path ):
-        if '\\' in log_path:
-            log_path = log_path.replace( '\\', '/' )
-        split_path = log_path.split( '/' )
-        dir_name = split_path.pop()
-        upper_dirname = split_path[-1]
-        upper_dir = '/'.join( split_path )
-        print( f'Directory "{ dir_name }" does not exist. Will create it if upper directory "{ upper_dirname }" is valid.\n' )
-        # upper folder check
-        if not path.exists( upper_dir ):                
-            print( f"Upper directory of { dir_name } does not exist either.\nPlease use an existing directory to store the logs." )
-            exit()        
-        else:
-            print( f"Creating { dir_name } under { upper_dir }\n" )
-            mkdir( log_path )
-            return True      
-    else:
-        print( f"Saving logs in { log_path }\n" )
-        return True
+	
+	print( f'Validating log directory: "{ log_path }"\n' )	
+	# set a unique delimiter regardless of platform (Linux\Windows)
+	
+	# folder check
+	if not path.exists( log_path ):
+		if '\\' in log_path:
+			log_path = log_path.replace( '\\', '/' )
+		split_path = log_path.split( '/' )
+		dir_name = split_path.pop()
+		upper_dirname = split_path[-1]
+		upper_dir = '/'.join( split_path )
+		print( f'Directory "{ dir_name }" does not exist. Will create it if upper directory "{ upper_dirname }" is valid.\n' )
+		# upper folder check
+		if not path.exists( upper_dir ):				
+			print( f"Upper directory of { dir_name } does not exist either.\nPlease use an existing directory to store the logs." )
+			exit()		
+		else:
+			print( f"Creating { dir_name } under { upper_dir }\n" )
+			mkdir( log_path )
+			return True	  
+	else:
+		print( f"Saving logs in { log_path }\n" )
+		return True
 
 
 def new_log_file( log_path, ymd_now ):  
-    # File name format: dirSync_2023-04-18.txt    
-    log_name = f"dirSync_{ ymd_now }.txt"
-    log_path = path.join( log_path, log_name )
-    log_file = open( log_path, 'a', encoding = 'UTF-8' )    
-    return log_file
+	# File name format: dirSync_2023-04-18.txt	
+	log_name = f"dirSync_{ ymd_now }.txt"
+	log_path = path.join( log_path, log_name )
+	log_file = open( log_path, 'a', encoding = 'UTF-8' )	
+	return log_file
+
+
+def log_it( logger, log_item, report=None, report_obj=None ):
+    
+    print( log_item )
+    logger.write( log_item )
+    
+    if report and report_obj:
+        report_obj = report
+	
+    return
 
 
 def generate_file_hex( target, rootdir, filename, blocksize=8192 ):
-    hh = md5()
-    with open( path.join( target, rootdir, filename ) , "rb" ) as f:
-        while buff := f.read( blocksize ):
-            hh.update( buff )
-    return hh.hexdigest()
+	hh = md5()
+	with open( path.join( target, rootdir, filename ) , "rb" ) as f:
+		while buff := f.read( blocksize ):
+			hh.update( buff )
+	return hh.hexdigest()
 
 
-def generate_hexmap( target, logger ):    
-    # hexmap report contains tuple of (action, bool) used for last stage of diff_hex()
-    # action = RM ||  RENAME || PASS || UPDATE || CREATE
-    hexmap = { 
-        'root': [],
-        'fname': [], 
-        'hex': [],
-        'report': []
-     }    
-    for directory in walk( target ):
-    # ( 0=dirname, 1=[folders], 2=[files] )   
+def generate_hexmap( target, logger ):	
+	# hexmap report contains tuple of (action, bool) used for last stage of diff_hex()
+	# action = RM ||  RENAME || PASS || UPDATE || CREATE
+	hexmap = { 
+		'root': [],
+		'fname': [], 
+		'hex': [],
+		'report': []
+	 }	
+	for directory in walk( target ):
+	# ( 0=dirname, 1=[folders], 2=[files] )   
 		
-		# separate starting from basename      
-        root = directory[0][ len( target ) : ]
-        
-        for fname in directory[2]:
-            hexmap[ 'root' ].append( root )
-            hexmap[ 'fname' ].append( fname )
-            hx = generate_file_hex( target, root, fname )
-            hexmap[ 'hex' ].append( hx )
-            
-            logger.write( root )
-            logger.write( fname )
-            logger.write( hx )
-            logger.write("\n\n--------------------------------------------------\n\n")            
-    return hexmap
-
-
-def refresh_path( dst_prop, prop ):
-
-    global client, client_hexmap
-    new_root, new_fname = None, None
-
-    for z in range( len( client_hexmap[ prop ] ) ): 
-               
-        if dst_prop == client_hexmap[ prop ][ z ]:
-            
-            new_fname = client_hexmap[ 'fname' ][ z ]            
-            new_root = client_hexmap[ 'root' ][ z ][ len( client ) : ]
-            # new_root holds the common root path that must be replicated
-            break
-        
-    return new_root, new_fname
-
-
-def rm_obsolete_dir( target, root, logger ):
-# action = RM ||  RENAME || PASS || UPDATE || CREATE      
+		# separate starting from basename	  
+		root = directory[0][ len( target ) : ]
+		
+		for fname in directory[2]:
+			hexmap[ 'root' ].append( root )
+			hexmap[ 'fname' ].append( fname )
+			hx = generate_file_hex( target, root, fname )
+			hexmap[ 'hex' ].append( hx )
+			
+			logger.write( f" {target} HEXMAP ".center (60, "-"))
+			logger.write( root )
+			logger.write( fname )
+			logger.write( hx )
+			logger.write("\n\n{60*'-'}\n\n")			
+	return hexmap
+	   
+	
+def rm_empty_dir( target, root, logger ):
+# action = RM ||  RENAME || PASS || UPDATE || CREATE	  
 	try:
 		removedirs( path.join( target, root ) )
-		log_item = f"Deleted directory { root }\n"
-		print( log_item )
-		logger.write( log_item )
+		log_it( logger, f"Deleted directory { root }\n" )
 		
 	except Exception as X:
-		log_item = f"Error: { X }\n"
-		print( log_item )
-		logger.write( log_item )
-    
+		log_it( logger, f"Error: { X }\n" )
+	
 	return
-        
+		
 
 def diff_hex( logger ):
-	'''
-    scandir
-    
-	same hex
-		same path
-		different filename || root || path
+	
+	# start from the client deepest root	
+	# if root not in cloud ['root'], review content recursively to remove\move\keep\update, then add in dir_to_delete[] for final cleanup	
 
-	no hex match
-		same path
-		same filename but diff root
-		no path match
-	'''	
-	global client, cloud, client_hexmap, cloud_hexmap  
+	# content scenario:	
+	# 	same hex
+	# 		same path
+	# 		different filename || root || path
 
-	for j in range( len( cloud_hexmap[ 'hex' ] ) ):
-        
+	# 	no hex match
+	# 		same path
+	# 		same filename but diff root
+	# 		no path match
+	
+	global client, cloud, client_hexmap, cloud_hexmap
+		
+	dir_to_rm = set()
+
+	# compare cloud against client
+	for hx_tgt in reversed( cloud_hexmap['hex'] ):
+	
+		j = cloud_hexmap['hex'].index( hx_tgt )
+										
 		dst_root = cloud_hexmap[ 'root' ][ j ]
 		dst_fn = cloud_hexmap[ 'fname' ][ j ] 
-		dst_hex = cloud_hexmap[ 'hex' ][ j ]                
-		dst_f = path.join( dst_root, dst_fn )        
-        
-		set_trace()
+		dst_hex = cloud_hexmap[ 'hex' ][ j ]				
+		dst_f = path.join( dst_root, dst_fn )
+
+		if dst_root not in client_hexmap[ 'root' ]:
+			dir_to_rm.add( dst_root )
 
 		# same hex
+		if dst_hex in client_hexmap['hex']:						
+			path_on_client = path.join( client, dst_f )
+						
 			# same path
-			# different filename || root || path
+			if path.exists( path_on_client ):
+				log_it( logger, f"PASS { dst_f }\n", cloud_hexmap[ 'report' ][ j ],'PASS' )
+				continue
 
+			# different filename || root || path
+			else:
+				# extract the corresponding full path on client side
+				for z in range( len( client_hexmap[ 'hex' ] ) ):
+					
+					if dst_hex == client_hexmap[ 'hex' ][ z ]:			
+						
+						new_fname = client_hexmap[ 'fname' ][ z ]			
+						new_root = client_hexmap[ 'root' ][ z ]
+
+						new_path = path.join( cloud, new_root, new_fname )
+						old_path = path.join( cloud, dst_f )
+						
+						rename( new_path, old_path )
+						log_it( logger, f"Renamed {old_path} to {new_path}\n", cloud_hexmap[ 'report' ][ j ], 'RENAMED' )
+			
 		# no hex match
+		else:			
 			# same path
 			# same filename but diff root
 			# no path match
 
-		report = pass_on_full_match( dst_hex, dst_root, dst_fn, logger )
-	
-		if report:
-            cloud_hexmap[ 'report' ][ j ] = report
-            continue
-    
-        # delete file with no matching hex & filename
-        report = del_no_matching_hex( dst_hex, dst_fn, dst_f, logger)
-        
-        if report != None:
-            cloud_hexmap[ 'report' ][ j ] = report
-            continue
-        
-        # replace file if hex not matching & but filename does; refresh dst path        
-        report = replace_by_fname( dst_hex, dst_fn, dst_f, logger )
-        
-        if report != None:
-            cloud_hexmap[ 'report' ][ j ] = report
-            continue            
-            
-        # rename file with matching hex and different path
-        report = rename_matching_hex(dst_hex, dst_root, dst_f, logger)
-        
-        if report != None:
-            cloud_hexmap[ 'report' ][ j ] = report
-            continue
-             
+	return dir_to_rm
  
 
 def dump_to_cloud( logger ):
 
-    global client, cloud
-    
-    log_item = f"Performing full sync\n"
-    print( log_item )
-    logger.write( log_item )
+	global client, cloud
+	
+	log_it( logger, f"Performing full sync\n" )
 
-    for item in listdir(client):    
-        try:
-            src = path.join( client, item )
-            dst = path.join( cloud, item )
-            
-            if path.isdir( src ):                
-                copytree(src, dst )
-            else:
-                copy2( src, dst )
-                
-            log_item = f"Copied { item }\n"
-            print( log_item )
-            logger.write( log_item )
-            
-        except Exception as X:
-            log_item = f"Error: { X }\n"
-            print( log_item )
-            logger.write( log_item )            
-    return
+	for item in listdir(client):	
+		try:
+			src = path.join( client, item )
+			dst = path.join( cloud, item )
+			
+			if path.isdir( src ):				
+				copytree(src, dst )
+			else:
+				copy2( src, dst )
+				
+			log_it( logger, f"Copied { item }\n" )
+			
+		except Exception as X:
+			log_it( logger,  f"Error: { X }\n" )
+	return
 
 
 def one_way_sync( logger ):
 # triggered by main if hexmap diff
-    
-    global client, cloud, client_hexmap, cloud_hexmap
-    
-    sync_start = datetime.now()
-     
-    log_item = f"Starting sync at { datetime.now().strftime( '%y-%m-%d %H:%M' ) }\n"
-    print( log_item )
-    logger.write( log_item )
-    
-    # get the source directory hash map
-    client_hexmap = generate_hexmap( client, logger )
-    
-    # full dump to cloud storage
-    if len( listdir( cloud ) ) == 0 and len( listdir( client ) ) != 0:
-        dump_to_cloud( logger )
-        cloud_hexmap = deepcopy(client_hexmap)
-        
-    else:
-        # get the destination directory hash map
-        cloud_hexmap = generate_hexmap( cloud, logger )    
-        # compare with cloud storage hexmap: root fname hex
-        diff_hex( logger )
-    
-    sync_finish = datetime.now()
-    
-    log_item = f"Finished sync at { datetime.now().strftime( '%y-%m-%d %H:%M' ) }\n"
-    print( log_item )
-    logger.write( log_item )
-    
-    return ( sync_finish - sync_start ).seconds
+	
+	global client, cloud, client_hexmap, cloud_hexmap
+	
+	sync_start = datetime.now()
+	 
+	log_it( logger, f"Starting sync at { datetime.now().strftime( '%y-%m-%d %H:%M' ) }\n" )
+	
+	# get the source directory hash map
+	client_hexmap = generate_hexmap( client, logger )
+	
+	# full dump to cloud storage
+	if len( listdir( cloud ) ) == 0 and len( listdir( client ) ) != 0:
+		dump_to_cloud( logger )
+		cloud_hexmap = deepcopy(client_hexmap)
+		
+	else:
+		# get the destination directory hash map
+		cloud_hexmap = generate_hexmap( cloud, logger )	
+		# compare with cloud storage hexmap: root fname hex
+		diff_hex( logger )
+	
+	sync_finish = datetime.now()
+	
+	log_it( logger, f"Finished sync at { datetime.now().strftime( '%y-%m-%d %H:%M' ) }\n" )
+	
+	return ( sync_finish - sync_start ).seconds
 
-    
-def main( log_path_set=False, logger=None ):    
-    
-    global client, cloud, log_path, interval    
-    
-    # log file timeframe
-    ymd_now = datetime.now().strftime( '%Y-%m-%d' )    
-    
-    # setup log file
-    if not log_path_set:      
-        log_path_set = setup_log_path( log_path )    
-    
-    # Check if the current log file matches the current date
-    if logger == None:
-        logger = new_log_file( log_path, ymd_now )    
-    else:
-        if ymd_now not in logger.name:
-            logger.close()
-            logger = new_log_file( log_path, ymd_now )
-    
-    # sync folders
-    sync_duration = one_way_sync( logger )    
-    
-    # determine last sync duration to cut from the interval sleep time until next sync 
-    sync_delta = sync_duration - interval
-    
-    log_item = f"Last sync took { sync_duration }\n"
-    print(log_item)
-    logger.write(log_item)
-    
-    if sync_delta <= 0:
-        sleep( interval )
-        main(log_path_set, logger)
-    else:
-        sleep( sync_delta )
-        main(log_path_set, logger)
+	
+def main( log_path_set=False, logger=None ):	
+	
+	global client, cloud, log_path, interval	
+	
+	# log file timeframe
+	ymd_now = datetime.now().strftime( '%Y-%m-%d' )	
+	
+	# setup log file
+	if not log_path_set:	  
+		log_path_set = setup_log_path( log_path )	
+	
+	# Check if the current log file matches the current date
+	if logger == None:
+		logger = new_log_file( log_path, ymd_now )	
+	else:
+		if ymd_now not in logger.name:
+			logger.close()
+			logger = new_log_file( log_path, ymd_now )
+	
+	# sync folders
+	sync_duration = one_way_sync( logger )	
+	
+	# determine last sync duration to cut from the interval sleep time until next sync 
+	sync_delta = sync_duration - interval
+	
+	log_it( logger, f"Last sync took { sync_duration }\n" )
+	
+	if sync_delta <= 0:
+		sleep( interval )
+		main(log_path_set, logger)
+	else:
+		sleep( sync_delta )
+		main(log_path_set, logger)
 
 
 if __name__ == '__main__':
-    main()
+	main()
