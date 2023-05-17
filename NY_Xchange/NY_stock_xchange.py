cec86214c5ae3c853a59ac0947bfd488da84c6b3
Eleuthar <flbujoreanu@gmail.com>
2023-05-17 15:06:30 +0300

    Update NY_stock_xchange.py

--- NY_Xchange/NY_stock_xchange.py
+++ NY_Xchange/NY_stock_xchange.py
@@ -1,61 +1,58 @@
-import xml.etree.ElementTree as x
-import pdb
+import xml.etree.ElementTree as xtree
+
+
+tree = None
 
 try:
-	tree = x.parse('nyse.xml')
+	tree = xtree.parse('nyse.xml')
+	root = tree.getroot()
+	tagz = root.getchildren()
+
+	# column header
+	header = list(tagz[0].keys())
+	header.insert(0, 'Company')
+
+	# get size of the longest text
+	txt = []
+	max_size = 0
+	reg_size = 10
+
+	for q in tagz:
+		txt.append(q.text)
+		if len(q.text) > max_size:
+			max_size = len(q.text) + 6
+
+	# header separator
+	sepLen = 0
+	for h in header:
+		sepLen += len(h) + 5
+
+	sepLen += max_size - len(header[0])
+	separator = '-' * sepLen
+
+	# print header, separator, text & attributes
+	for q in header:
+		if q != 'Company':
+			if header.index(q) == 4:
+				print(q.upper().ljust(reg_size, " "))
+			else:
+				print(q.upper().ljust(reg_size, " "), end='')
+		else:
+			print(q.upper().ljust(max_size, " "), end="")
+
+	print(separator)
+
+	# pdb.set_trace()
+	for q in tagz:
+		for xtree in range(len(header)):
+			if xtree == 0:
+				print(q.text.ljust(max_size, ' '), end='')
+			else:
+				print(q.attrib[header[xtree]].ljust(10, " "), end='')
+		print('\n')
+
 except FileNotFoundError as fe:
 	print(repr(fe))
-except x.ParseError as pe:
+
+except xtree.ParseError as pe:
 	print(repr(pe))
-	
-root = tree.getroot()
-tagz = root.getchildren()
-
-# column header
-header = tagz[0].keys()
-header.insert(0,'Company')
-
-
-# get size of the longest text
-txt = []
-maxSize = 0
-regSize = 10
-
-for q in tagz:
-	txt.append(q.text)
-	if len(q.text) > maxSize:
-		maxSize = len(q.text) + 6
-
-
-# header separator
-sepLen = 0
-for h in header:
-	sepLen += len(h)+5  
-
-sepLen += maxSize - len(header[0])
-separator = '-' * sepLen
-
-
-# print header, separator, text & attributes
-for q in header:
-	if q != 'Company':
-		if header.index(q) == 4:
-			print(q.upper().ljust(regSize, " "))
-		else:
-			print(q.upper().ljust(regSize, " "), end='')
-	else:
-		print(q.upper().ljust(maxSize, " "), end="")
-
-print(separator)
-
-#pdb.set_trace()
-for q in tagz:
-	for x in range(len(header)):
-		if x == 0:
-			print(q.text.ljust(maxSize,' '), end='')
-		else:
-			print(q.attrib[header[x]].ljust(10, " ") ,end='')
-	print('\n')
-
-
-
