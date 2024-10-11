abd64ed7ebbe541e78de4eeafa76fd04ba544fb7
Eleuthar <flbujoreanu@gmail.com>
2024-10-12 01:04:51 +0300

    Delete DRF tut directory

--- DRF tut/rest_api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ['pk']
-    list_display = ('pk', '__str__',)
-
-admin.site.register(Post, PostAdmin)
