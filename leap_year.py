ad2b8ed4a7d1a4fb4b47bb6f399998b4ee6c6421
Eleuthar <flbujoreanu@gmail.com>
2026-06-24 13:15:49 +0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,6 @@
 
 def is_year_leap(year):
-    if year % 400 == 0 or (year % 100 != 0 and year % 4 == 0):
+    if year % 400 == 0 and (year % 100 != 0 and year % 4 == 0):
         return True
     else:
         return False
